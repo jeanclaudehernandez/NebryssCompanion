b624668f40c4d52313e138170d1751de2bbcf4c5
Jean Hernandez <>
2025-03-30 18:13:13 -0300

    added angular material tooltip

--- NebryssCompanion/src/app/app.config.ts
+++ NebryssCompanion/src/app/app.config.ts
@@ -3,7 +3,8 @@ import { provideHttpClient } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideHttpClient(),]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideHttpClient(), provideAnimationsAsync(),]
 };
