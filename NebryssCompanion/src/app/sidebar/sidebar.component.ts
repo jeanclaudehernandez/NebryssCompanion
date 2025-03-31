202f498642415a938e960ad14d506d3747589328
Jean Hernandez <>
2025-03-31 17:06:37 -0300

    added sidebar closing when clicking outside

--- NebryssCompanion/src/app/sidebar/sidebar.component.ts
+++ NebryssCompanion/src/app/sidebar/sidebar.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output, ViewChild, ElementRef, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-sidebar',
@@ -9,8 +9,23 @@ import { Component, EventEmitter, Output } from '@angular/core';
   styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent {
+  @ViewChild('sidebar') sidebarElement!: ElementRef;
+  @ViewChild('burger') burgerElement!: ElementRef;
   @Output() viewChange = new EventEmitter<'players' | 'bestiary' | 'items' | 'shops' | 'lore'>();
   isOpen = false;
+
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: MouseEvent): void {
+    if (this.isOpen && !this.isClickInside(event)) {
+      this.isOpen = false;
+    }
+  }
+
+  private isClickInside(event: MouseEvent): boolean {
+    const clickedInsideSidebar = this.sidebarElement?.nativeElement.contains(event.target as Node);
+    const clickedInsideBurger = this.burgerElement?.nativeElement.contains(event.target as Node);
+    return clickedInsideSidebar || clickedInsideBurger;
+  }
 
   toggleMenu() {
     this.isOpen = !this.isOpen;
