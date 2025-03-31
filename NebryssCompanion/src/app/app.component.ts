dbd4a4be193bfe96e1790826a1df7c523c9e5e07
Jean Hernandez <>
2025-03-30 21:52:29 -0300

    added bestiary support

--- NebryssCompanion/src/app/app.component.ts
+++ NebryssCompanion/src/app/app.component.ts
@@ -1,17 +1,87 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, JsonPipe } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { PlayerListComponent } from './player-list/player-list.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { DataService } from './data.service';
+import { PlayerDetailComponent } from './player-detail/player-detail.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, HttpClientModule, PlayerListComponent],
+  imports: [
+    CommonModule,
+    HttpClientModule,
+    PlayerListComponent,
+    SidebarComponent,
+    PlayerDetailComponent,
+    FormsModule,
+    JsonPipe
+  ],
   template: `
-    <app-player-list></app-player-list>
+    <app-sidebar (viewChange)="onViewChange($event)"></app-sidebar>
+    
+    <div class="content-area">
+      <!-- Players View -->
+      <app-player-list *ngIf="currentView === 'players'"></app-player-list>
+
+      <!-- Bestiary Selection -->
+      <div *ngIf="currentView === 'bestiary'" class="bestiary-select-container">
+        <h2>Select Bestiary Creature</h2>
+        <select [(ngModel)]="selectedCreatureId" (change)="onCreatureSelected()">
+          <option [value]="null">Select a creature</option>
+          <option *ngFor="let creature of bestiary" [value]="creature.id">
+            {{ creature.name }} ({{ creature.faction }})
+          </option>
+        </select>
+      </div>
+      <!-- Creature Detail View -->
+      <app-player-detail 
+        *ngIf="selectedCreature"
+        [character]="selectedCreature"
+        [weaponsData]="weaponsData"
+        [characterType]="'mob'"
+        [itemsData]="itemsData"
+        [weaponRulesData]="weaponRulesData">
+      </app-player-detail>
+    </div>
   `,
-  styles: []
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'mistborn-kill-team';
+  currentView: 'players' | 'bestiary' = 'players';
+  bestiary: any[] = [];
+  selectedCreatureId: number | null = null;
+  selectedCreature: any = null;
+  weaponsData: any;
+  itemsData: any;
+  weaponRulesData: any[] = [];
+  menuOpen = false;
+
+  constructor(private dataService: DataService) {}
+
+  ngOnInit() {
+    this.dataService.getAllData().subscribe(response => {
+      this.bestiary = response.bestiary;
+      this.itemsData = response.items;
+      this.weaponsData = response.weapons;
+      this.weaponRulesData = response.weaponRules
+    });
+  }
+
+  onViewChange(view: 'players' | 'bestiary') {
+    this.currentView = view;
+    this.selectedCreature = null; // Reset selected creature when switching views
+    this.menuOpen = view === 'bestiary';
+  }
+
+  onCreatureSelected() {
+    if (this.selectedCreatureId) {
+      this.selectedCreature = this.bestiary.find(c => c.id === Number(this.selectedCreatureId));
+      this.dataService.getWeapons().subscribe(weapons => {
+        this.weaponsData = weapons;
+      });
+    }
+  }
 }
---
+++ NebryssCompanion/src/app/sidebar/sidebar.component.ts
@@ -0,0 +1,28 @@
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-sidebar',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.css']
+})
+export class SidebarComponent {
+  @Output() viewChange = new EventEmitter<'players' | 'bestiary'>();
+  isOpen = false;
+
+  toggleMenu() {
+    this.isOpen = !this.isOpen;
+  }
+
+  showPlayers() {
+    this.viewChange.emit('players');
+    this.isOpen = false;
+  }
+
+  showBestiary() {
+    this.viewChange.emit('bestiary');
+    this.isOpen = false;
+  }
+}
