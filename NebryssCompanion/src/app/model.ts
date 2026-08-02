035b8580594e401b652545b181d0e7174e8c7109
Jean <jeanhernandezmeze@gmail.com>
2026-08-02 15:42:33 -0300

    <feature> updated shops UI

--- NebryssCompanion/src/app/model.ts
+++ NebryssCompanion/src/app/model.ts
@@ -312,6 +312,7 @@ export interface AlteredState {
     id: number;
     name: string;
     owner: number;
+    locationId?: number;
     location: string;
     locationName: string;
     description?: string;
